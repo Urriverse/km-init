34f8316ebcce0e9520989b7e646156ee7512b0a3
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 22:01:05 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 #![no_std] #![no_main] #![feature(decl_macro)]
-
 #[allow(unused)] #[macro_use] extern crate nk;
 
-nk::util::meta!("km-init");
+meta!
+{
+    "km-init"
+}
 
-#[cfg(not(test))] nk::macros::panic_handler![];
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() {
-    // greetings
+entry! {
     panic!("Hey there!");
-    // nk::exit!();
 }
