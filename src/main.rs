38831991ce0849ba29ba9771ada519fad654c64d
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 13:46:23 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![no_main]
 #![no_std]
 
-#[allow(unused)] #[macro_use] pub extern crate nanokit;
+// #[allow(unused)] #[macro_use] pub extern crate nanokit;
 
-allocator!{}
+// allocator!{}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn _start() {
+    loop {
+        //
+    }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn _ph(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        //
+    }
+}
