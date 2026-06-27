38348baab7a1b71804d351136cf71411020d0044
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 18:59:56 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,9 @@ pub mod nk; // include nanokit
 
 Mod! { "km-init" }  // declare module name (this macro also adds custom prelude)
 
-fn example() {
-    error!("unimplemented");
-}
-
 // entry point
 fn main() -> i32 {
     // greetings
     warn!("Hey there!");
-
-    // export symbol
-    dlexport!  [ @ b"example" => example ];
-
-    // link with symbol
-    let sym = dlink![ @ b"example"; "Link failed" ];
-
-    // use symbol
-    dluse! [ sym => fn() ] ();
-
     10
 }
