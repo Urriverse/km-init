7c1a46a16fb4e7b7363c26d04aee9858b36aacd1
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 01:57:46 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,12 @@ meta! { "km-init" }
 
 entry! {
     info!("Hey there!");
+    
+    if let Some(x) = nk::KeVtDeviceNew("kbd0") {
+        info!("Success: created KeDevice `kbd0`! :: {:?}", x);
+    } else {
+        error!("Failed to create KeDevice `kbd0`");
+    }
+    
     exit!(1488);
 }
