a231d0f0a3547506ab8181f3e165f20e69189395
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 06:54:30 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-#![no_std] #![no_main]
+#![no_std] #![no_main] #![crate_type = "dylib"]
 #[macro_use] extern crate nk;
 
 meta! { "km-init" }
 
+type KeTest = fn();
+
+nk::Ke! { KeTest or || () }
+
 entry! {
     info!("Hey there!");
+
+    KeTest();
     
     if let Some(x) = nk::KeVtDeviceNew("kbd0") {
         info!("Success: created KeDevice `kbd0`! :: {:p}", core::ptr::addr_of!(x));
