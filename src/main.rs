617f6c3e6575552316e0291af39919e8b4c72fe5
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 06:39:20 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ entry! {
     if let Some(x) = nk::KeVtDeviceNew("kbd0") {
         info!("Success: created KeDevice `kbd0`! :: {:?}", x);
     } else {
-        error!("Failed to create KeDevice `kbd0`");
+        error!("Failed to create KeDevice `kbd0`!");
     }
     
     exit!(1488);
