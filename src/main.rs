48a768dea27dfb70385dc58da795dce5bc43c94e
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 11:10:12 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@
 //     }
 // }
 
-#[allow(non_snake_case)]
-fn __stub_ExecYield() { error!("ExecYield not provided"); }
+// #[allow(non_snake_case)]
+// fn __stub_ExecYield() { error!("ExecYield not provided"); }
 
-#[used]
-#[allow(non_upper_case_globals)]
-#[unsafe(export_name = concat!("KiExecYield"))]
-static _ExecYield: Import = Import(__stub_ExecYield as *const (), 0);
+// #[used]
+// #[allow(non_upper_case_globals)]
+// #[unsafe(export_name = concat!("KiExecYield"))]
+// static _ExecYield: Import = Import(__stub_ExecYield as *const (), 0);
 
-#[allow(non_snake_case)]
-#[inline(always)]
-pub fn ExecYield() {
-    (unsafe{core::mem::transmute::<_, fn ()>(_ExecYield.0 )})()
-}
+// #[allow(non_snake_case)]
+// #[inline(always)]
+// pub fn ExecYield() {
+//     (unsafe{core::mem::transmute::<_, fn ()>(_ExecYield.0 )})()
+// }
 
 entry! {
     mod "km-init";
 
-    ExecYield();
+    nk::ExecYield();
 
     info!("Hey there!");
 
