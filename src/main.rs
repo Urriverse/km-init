b0d81e663902107168baad946ab926646ea58d47
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 11:11:41 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,4 @@
-#![no_std] #![no_main] #[macro_use] extern crate nk; use ketypes::*;
-
-// Import! {
-//     pub fn ExecYield() where kernel 0.0 {
-//         error!("ExecYield not provided");
-//     }
-// }
-
-// #[allow(non_snake_case)]
-// fn __stub_ExecYield() { error!("ExecYield not provided"); }
-
-// #[used]
-// #[allow(non_upper_case_globals)]
-// #[unsafe(export_name = concat!("KiExecYield"))]
-// static _ExecYield: Import = Import(__stub_ExecYield as *const (), 0);
-
-// #[allow(non_snake_case)]
-// #[inline(always)]
-// pub fn ExecYield() {
-//     (unsafe{core::mem::transmute::<_, fn ()>(_ExecYield.0 )})()
-// }
+#![no_std] #![no_main] #[macro_use] extern crate nk;
 
 entry! {
     mod "km-init";
